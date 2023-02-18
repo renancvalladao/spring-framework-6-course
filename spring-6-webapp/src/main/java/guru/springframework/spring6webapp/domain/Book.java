62c62be5e73d64d0cce00a54ee91659a0757a542
Renan Valladão <renancvalladao@gmail.com>
2023-02-18 20:25:16 -0300

    Adding relationships

--- spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Book.java
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Book.java
@@ -1,9 +1,8 @@
 package guru.springframework.spring6webapp.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 public class Book {
@@ -13,6 +12,10 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
 
     public Long getId() {
         return this.id;
@@ -36,6 +39,14 @@ public class Book {
 
     public void setIsbn(String isbn) {
         this.isbn = isbn;
+    }
+
+    public Set<Author> getAuthors() {
+        return this.authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
     }
 
 }
