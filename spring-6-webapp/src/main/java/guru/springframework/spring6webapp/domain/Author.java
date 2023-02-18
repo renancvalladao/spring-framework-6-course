aabdf9abcabbb1772c31762974aa15958c786a2c
Renan Valladão <renancvalladao@gmail.com>
2023-02-18 20:24:49 -0300

    Adding JPA entities

---
+++ spring-6-webapp/src/main/java/guru/springframework/spring6webapp/domain/Author.java
@@ -0,0 +1,41 @@
+package guru.springframework.spring6webapp.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
