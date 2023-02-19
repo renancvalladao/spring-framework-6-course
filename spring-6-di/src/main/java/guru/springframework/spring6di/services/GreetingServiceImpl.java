b0f9bc5dfa364c4968c45698f336e48fb16d7fe1
Renan Valladão <renancvalladao@gmail.com>
2023-02-19 18:35:50 -0300

    Adding example of dependencies without injection

---
+++ spring-6-di/src/main/java/guru/springframework/spring6di/services/GreetingServiceImpl.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring6di.services;
+
+public class GreetingServiceImpl implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello Everyone From Base Service!!!";
+    }
+
+}
