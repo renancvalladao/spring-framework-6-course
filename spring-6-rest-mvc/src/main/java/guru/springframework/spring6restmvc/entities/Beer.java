203330252eb620031f4ea05b35d2239913fd7168
Renan Valladão <renancvalladao@gmail.com>
2023-02-28 19:33:18 -0300

    Adding database constraint validation on column length

--- spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
+++ spring-6-rest-mvc/src/main/java/guru/springframework/spring6restmvc/entities/Beer.java
@@ -4,6 +4,7 @@ import guru.springframework.spring6restmvc.model.BeerStyle;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
 
@@ -26,14 +27,17 @@ public class Beer {
     private UUID id;
     @Version
     private Integer version;
-    @NotBlank
-    @NotNull
-    private String beerName;
     @NotNull
     private BeerStyle beerStyle;
     @NotBlank
     @NotNull
+    @Size(max = 255)
     private String upc;
+    @NotBlank
+    @NotNull
+    @Size(max = 50)
+    @Column(length = 50)
+    private String beerName;
     private Integer quantityOnHand;
     @NotNull
     private BigDecimal price;
--- spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
+++ spring-6-rest-mvc/src/test/java/guru/springframework/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -2,6 +2,7 @@ package guru.springframework.spring6restmvc.repositories;
 
 import guru.springframework.spring6restmvc.entities.Beer;
 import guru.springframework.spring6restmvc.model.BeerStyle;
+import jakarta.validation.ConstraintViolationException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -9,12 +10,26 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import java.math.BigDecimal;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testSaveBeerTooLong() {
+        assertThrows(ConstraintViolationException.class, () -> {
+            this.beerRepository.save(Beer.builder()
+                    .beerName("My Beer01234567890123456789012345678901234567890123456789")
+                    .beerStyle(BeerStyle.PALE_ALE)
+                    .upc("23432343234")
+                    .price(new BigDecimal("11.99"))
+                    .build());
+            this.beerRepository.flush();
+        });
+    }
 
     @Test
     void testSaveBeer() {
